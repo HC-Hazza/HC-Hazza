0ae5f27199ecd458b0ea88bc7a3449b7912c887e
HC-Hazza <hacurtis02@gmail.com>
2025-02-18 10:47:59 +0000

    Create some-bat-about-carrot-cake.cpp

---
+++ Practise Code/C++-Practise-Files/Cplusplus-CodeLab/some-bat-about-carrot-cake.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s1 = "Carrot";
+	string s2 = "Cake";
+		if (s1 == s2) {
+			cout << "Strings match" << endl;
+		}
+		else {
+			cout << "Strings do not match" << endl;
+		}
+	return 0;
+}
