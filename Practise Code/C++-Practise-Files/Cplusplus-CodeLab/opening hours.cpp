8a115baaabd94665aebc9e465b78fe58d405b70a
HC-Hazza <hacurtis02@gmail.com>
2025-02-18 10:43:37 +0000

    Create opening hours.cpp

---
+++ Practise Code/C++-Practise-Files/Cplusplus-CodeLab/opening hours.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int time;
+	cout << "What time is it?" << endl;
+	cin >> time;
+	switch (time) {
+	case 0:
+		cout << "Closed.";
+		break;
+	case 1:
+		cout << "Closed.";
+		break;
+	case 2:
+		cout << "Closed.";
+		break;
+	case 3:
+		cout << "Closed.";
+		break;
+	case 4:
+		cout << "Closed.";
+		break;
+	case 5:
+		cout << "Closed.";
+		break;
+	case 6:
+		cout << "Closed.";
+		break;
+	case 7:
+		cout << "Closed.";
+		break;
+	case 8:
+		cout << "Closed.";
+		break;
+	case 9:
+		cout << "Opening.";
+		break;
+	case 10:
+		cout << "Open.";
+		break;
+	case 11:
+		cout << "Open.";
+		break;
+	case 12:
+		cout << "Lunch break has begun.";
+		break;
+	case 13:
+		cout << "Lunch break has ended.";
+		break;
+	case 14:
+		cout << "Open.";
+		break;
+	case 15:
+		cout << "Open.";
+		break;
+	case 16:
+		cout << "Open.";
+		break;
+	case 17:
+		cout << "Open.";
+		break;
+	case 18:
+		cout << "Open.";
+		break;
+	case 19:
+		cout << "Closing.";
+		break;
+	case 20:
+		cout << "Closed.";
+		break;
+	case 21:
+		cout << "Closed.";
+		break;
+	case 22:
+		cout << "Closed.";
+		break;
+	case 23:
+		cout << "Closed.";
+		break;
+	}
+	return 0;
+}
